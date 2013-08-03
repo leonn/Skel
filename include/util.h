fce7684b8f9b2c049a2d70063d871abef1477a9a
leonn <leonn.paiva@gmail.com>
2013-08-03 09:39:08 -0300

    flip image function

--- include/util.h
+++ include/util.h
@@ -11,4 +11,10 @@ SDL_Surface * loadImage(const char* img);
 
 bool handleColision(SDL_Rect box1, SDL_Rect box2);
 
+Uint32 get_pixel32( SDL_Surface *surface, int x, int y );
+
+void put_pixel32( SDL_Surface *surface, int x, int y, Uint32 pixel );
+
+SDL_Surface *flipImage( SDL_Surface *surface, int flags );
+
 #endif
